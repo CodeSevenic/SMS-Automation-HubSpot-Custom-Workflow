56e1b1186fa527eae69668ee90df82e7943f1be7
CodeSevenic <sevenicbookings@gmail.com>
2023-02-28 12:20:51 +0200

    Nettel code update

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,3 +35,50 @@ exports.register = (req, res) => {
   // TODO: save user data to database or perform other actions
   res.send(`Registration successful. Username: ${username}, Email: ${email}`);
 };
+
+let userData;
+
+exports.login = (req, res) => {
+  const { username, password } = req.body;
+  userData = {
+    username: username,
+    password: password,
+  };
+
+  console.log(userData);
+  // Find user by username and password
+  // const user = users.find((u) => u.username === username && u.password === password);
+
+  // if (user) {
+  //   // Successful login
+  //   res.send('Welcome ' + username + '!');
+  // } else {
+  //   // Invalid login
+  //   res.status(401).send('Invalid username or password');
+  // }
+  if (username && password) {
+    res.redirect('/view');
+  }
+
+  res.status(200).send();
+};
+
+exports.renderView = async (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`<h2>SMS Automation</h2>`);
+  let authorized = await isAuthorized(req.sessionID);
+  console.log(authorized);
+  if (authorized) {
+    console.log('Other hello: ', userData);
+    const accessToken = await getAccessToken(req.sessionID, userData);
+    hubspotClient = new hubspot.Client({ accessToken: `${accessToken}` });
+    const contact = await resContacts(accessToken);
+    displayContactName(res, contact);
+    // recentUpdatedProperties(accessToken);
+    // createCustomWorkflow();
+    // getAllCustomActions();
+  } else {
+    res.write(`<a href="/install"><h3>Install the app</h3></a>`);
+  }
+  res.end();
+};

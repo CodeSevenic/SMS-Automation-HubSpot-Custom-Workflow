4bcf628ef65a4c0f25c4ed8fe76dd54023c2bdb9
CodeSevenic <sevenicbookings@gmail.com>
2023-02-28 12:08:14 +0200

    Nettel code update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,37 @@
-﻿
+﻿const { validationResult } = require('express-validator');
+
+exports.registerForm = (req, res) => {
+  res.send(/*template*/ `
+    <h1>Registration Form</h1>
+    <form method="POST" action="/register">
+      <label>
+        Username:
+        <input type="text" name="username">
+      </label>
+      <br>
+      <label>
+        Email:
+        <input type="email" name="email">
+      </label>
+      <br>
+      <label>
+        Password:
+        <input type="password" name="password">
+      </label>
+      <br>
+      <button type="submit">Register</button>
+    </form>
+  `);
+};
+
+exports.register = (req, res) => {
+  // Check for validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  // If there are no validation errors, save user data to database or perform other actions
+  const { username, email, password } = req.body;
+  // TODO: save user data to database or perform other actions
+  res.send(`Registration successful. Username: ${username}, Email: ${email}`);
+};
--- validators/auth.js
+++ validators/auth.js
@@ -1,1 +1,12 @@
 ﻿const { body, validationResult } = require('express-validator');
+// Validate username
+exports.userSignupValidator = [
+  body('username').notEmpty().withMessage('Username is required.'),
+  body('username').isLength({ min: 5 }).withMessage('Username must be at least 5 characters long.'),
+  // Validate email
+  body('email').notEmpty().withMessage('Email is required.'),
+  body('email').isEmail().withMessage('Invalid email address.'),
+  // Validate password
+  body('password').notEmpty().withMessage('Password is required.'),
+  body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long.'),
+];

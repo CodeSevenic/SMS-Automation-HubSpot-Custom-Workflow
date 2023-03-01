411a8f8a3958d6c02b68bffe6f4e30ecbf25be99
CodeSevenic <sevenicbookings@gmail.com>
2023-03-01 12:42:06 +0200

    Nettel code update

--- index.js
+++ index.js
@@ -8,19 +8,8 @@ const cors = require('cors');
 const port = process.env.PORT || 8000;
 const { exchangeForTokens } = require('./oauth/oauth');
 const { main } = require('./twilio/twilio');
-const {
-  renderView,
-  loginPage,
-  registerForm,
-  register,
-  attemptLogin,
-  welcomePage,
-  hubspotActions,
-} = require('./controllers/auth');
-
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-  throw new Error('Missing CLIENT_ID or CLIENT_SECRET environment variable.');
-}
+const { register, attemptLogin, hubspotActions } = require('./controllers/auth');
+const { install, oauthCallback } = require('./controllers/hubspot');
 
 // app.use(express.json());
 app.use(express.json({ limit: '10kb' }));
@@ -35,52 +24,15 @@ app.use(
   })
 );
 
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-  throw new Error('Missing CLIENT_ID or CLIENT_SECRET environment variable.');
-}
-
-//===========================================================================//
-//  HUBSPOT APP CONFIGURATION
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
-// Scopes for this app will default to `crm.objects.contacts.read`
-// To request others, set the SCOPE environment variable instead
-let SCOPES = ['crm.objects.contacts.read'];
-if (process.env.SCOPE) {
-  SCOPES = process.env.SCOPE.split(/ |, ?|%20/).join(' ');
-}
-
-// On successful install, users will be redirected to /oauth-callback
-const REDIRECT_URI = `http://localhost:${port}/oauth-callback`;
-
-//===========================================================================//
-
 //================================//
 //   Running the OAuth 2.0 Flow   //
 //================================//
 
 // Step 1
 
-// Build the authorization URL to redirect a user
-// to when they choose to install the app
-
-const authUrl =
-  'https://app.hubspot.com/oauth/authorize' +
-  `?client_id=${encodeURIComponent(CLIENT_ID)}` + // app's client ID
-  `&scope=${encodeURIComponent(SCOPES)}` + // scopes being requested by the app
-  `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`; // where to send the user after the consent page
-
 // Redirect the user from the installation page to
 // the authorization URL
-app.get('/install', (req, res) => {
-  console.log('');
-  console.log('=== Initiating OAuth 2.0 flow with HubSpot ===');
-  console.log('');
-  console.log("===> Step 1: Redirecting user to your app's OAuth URL");
-  res.redirect(authUrl);
-  console.log('===> Step 2: User is being prompted for consent by HubSpot');
-});
+app.get('/install', install);
 
 // Step 2
 // The user is prompted to give the app access to the requested
@@ -90,35 +42,7 @@ app.get('/install', (req, res) => {
 // Step 3
 // Receive the authorization code from the OAuth 2.0 Server,
 // and process it based on the query parameters that are passed
-app.get('/oauth-callback', async (req, res) => {
-  console.log('===> Step 3: Handling the request sent by the server');
-
-  // Received a user authorization code, so now combine that with the other
-  // required values and exchange both for an access token and a refresh token
-  if (req.query.code) {
-    console.log('       > Received an authorization token');
-
-    const authCodeProof = {
-      grant_type: 'authorization_code',
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      redirect_uri: REDIRECT_URI,
-      code: req.query.code,
-    };
-
-    // Step 4
-    // Exchange the authorization code for an access token and refresh token
-    console.log('===> Step 4: Exchanging authorization code for an access token and refresh token');
-    const token = await exchangeForTokens(req.sessionID, authCodeProof);
-    if (token.message) {
-      return res.redirect(`/error?msg=${token.message}`);
-    }
-
-    // Once the tokens have been retrieved, use them to make a query
-    // to the HubSpot API
-    res.redirect(`/`);
-  }
-});
+app.get('/oauth-callback', oauthCallback);
 
 //User Registration
 app.post('/register', register);
